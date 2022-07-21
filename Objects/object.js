59a4af8bf5467a2036c2471785005fe68fb1bb0d
margari25 <budryte.m@mail.ru>
2022-07-21 20:40:27 +0300

    20220721

--- Objects/object.js
+++ Objects/object.js
@@ -195,6 +195,16 @@ const grow = x => {
   return res;
 };
 
+function correct(s) {
+  s = s.split('');
+  for (var i = 0; i < s.length; i++) {
+    if (s[i] === '5') s[i] = 'S';
+    else if (s[i] === '0') s[i] = 'O';
+    else if (s[i] === '1') s[i] = 'I';
+  }
+  return s.join('');
+}
+
 function countBy(x, n) {
   var z = [];
   for (i = 1; i <= n; i++) {
